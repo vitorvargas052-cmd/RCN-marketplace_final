6e2e1bca90e096e74b0cbe6c78b1983ca785e16f
vitorvargas052-cmd <vitorvargas052@gmail.com>
2025-10-29 12:58:10 -0300

    Remove swcMinify and simplify config options

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com", "supabase.co"],
   },
