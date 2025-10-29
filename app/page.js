9bb203bcc13ad28792419822044a5379842fcb97
vitorvargas052-cmd <vitorvargas052@gmail.com>
2025-10-29 12:56:22 -0300

    Create HomePage component for main layout

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "#0d0d0d", color: "#fff", flexDirection: "column" }}>
+      <h1>🚀 RCN Marketplace</h1>
+      <p>Seu site está rodando no Next.js 15!</p>
+    </main>
+  );
+  }
